7d0f6ae5b37e54d4fbb63bf56b0dab70b8dfb2d1
Alexander Rowland <alexander.c.rowland@gmail.com>
2016-11-01 23:44:02 -0400

    Shell sort working with gap of 2

--- shell_sort.py
+++ shell_sort.py
@@ -3,14 +3,20 @@ from insertion_sort import insertion_sort
 
 def shell_sort(array):
     n = len(array)
-    h = 1
-    while h < n:
-        h = 3 * h + 1
-    while h > 0:
-        h //= 3
-        for k in range(1, h):
-            insertion_sort(array[k:h:n])
+    gap = len(array) // 2
+    while gap > 0:
+        for i in range(gap, n):
+            val = array[i]
+            j = i
+            while j >= gap and array[j - gap] > val:
+                array[j] = array[j - gap]
+                j -= gap
+            array[j] = val
+        gap //= 2
 
 
 test = [1, 2, 3, 4, 14, 7, 3, 2, 101, 78]
-print(shell_sort(test) == sorted(test))
+
+print(test)
+shell_sort(test)
+print(test == sorted(test))
